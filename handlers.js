9153871bc69bff2772f976a9ce207001da9703ad
Manuel Augusto Bravard <manubravard@gmail.com>
2023-07-08 10:10:27 -0300

    not all toasts

--- handlers.js
+++ handlers.js
@@ -43,6 +43,7 @@ export async function handleResult({ results }) {
 
   const totalCount = Object.keys($EXPECTED_COLORS).length;
 
+  // (some help for the user)
   for (let index = 0; index < filteredColorsArray1.length; index++) {
     const color = filteredColorsArray1[index];
     if ($EXPECTED_COLORS[color] && !$COLORS_FOUNDED.includes(color)) {
@@ -59,7 +60,7 @@ export async function handleResult({ results }) {
         handleStop();
         ToastService.showGreatJob();
       }
-    } else {
+    } else if (!$EXPECTED_COLORS[color]) {
       ToastService.showErrorToast(`
       <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="24" height="24" viewBox="0 0 24 24">
         <path
@@ -68,6 +69,7 @@ export async function handleResult({ results }) {
       Transcripted: ${color}`);
     }
   }
+  // the literal transcriptcion
   for (let index = 0; index < filteredColorsArray2.length; index++) {
     const color = filteredColorsArray2[index];
     if ($EXPECTED_COLORS[color] && !$COLORS_FOUNDED.includes(color)) {
@@ -84,15 +86,17 @@ export async function handleResult({ results }) {
         handleStop();
         ToastService.showGreatJob();
       }
-    } else {
-      ToastService.showErrorToast(`
-      <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="24" height="24" viewBox="0 0 24 24">
-        <path
-            d="M10.872 6.831l1.695 3.904 3.654-1.561-1.79 3.426 3.333.954-3.417 1.338 2.231 4.196-4.773-2.582-2.869 2.287.413-3.004-3.792-.726 2.93-1.74-1.885-2.512 3.427.646.843-4.626zm-.786-6.831l-1.665 9.119-6.512-1.228 3.639 4.851-5.548 3.294 7.108 1.361-.834 6.076 5.742-4.577 9.438 5.104-4.288-8.064 6.834-2.677-6.661-1.907 3.25-6.22-6.98 2.982-3.523-8.114z" />
-    </svg>
-      Transcripted: ${color}`);
     }
+    // else if (!$EXPECTED_COLORS[color]) {
+    //   ToastService.showErrorToast(`
+    //   <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="24" height="24" viewBox="0 0 24 24">
+    //     <path
+    //         d="M10.872 6.831l1.695 3.904 3.654-1.561-1.79 3.426 3.333.954-3.417 1.338 2.231 4.196-4.773-2.582-2.869 2.287.413-3.004-3.792-.726 2.93-1.74-1.885-2.512 3.427.646.843-4.626zm-.786-6.831l-1.665 9.119-6.512-1.228 3.639 4.851-5.548 3.294 7.108 1.361-.834 6.076 5.742-4.577 9.438 5.104-4.288-8.064 6.834-2.677-6.661-1.907 3.25-6.22-6.98 2.982-3.523-8.114z" />
+    // </svg>
+    //   Transcripted: ${color}`);
+    // }
   }
+  // the last combination (some help for the user)
   if (
     $EXPECTED_COLORS[filteredColor3] &&
     !$COLORS_FOUNDED.includes(filteredColor3)
@@ -108,13 +112,14 @@ export async function handleResult({ results }) {
       console.log(correctCount);
       handleStop();
       ToastService.showGreatJob();
-    } else {
-      ToastService.showErrorToast(`
-      <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="24" height="24" viewBox="0 0 24 24">
-        <path
-            d="M10.872 6.831l1.695 3.904 3.654-1.561-1.79 3.426 3.333.954-3.417 1.338 2.231 4.196-4.773-2.582-2.869 2.287.413-3.004-3.792-.726 2.93-1.74-1.885-2.512 3.427.646.843-4.626zm-.786-6.831l-1.665 9.119-6.512-1.228 3.639 4.851-5.548 3.294 7.108 1.361-.834 6.076 5.742-4.577 9.438 5.104-4.288-8.064 6.834-2.677-6.661-1.907 3.25-6.22-6.98 2.982-3.523-8.114z" />
-    </svg>
-      Transcripted: ${filteredColor3}`);
     }
+    // else if (!$EXPECTED_COLORS[filteredColor3]) {
+    //   ToastService.showErrorToast(`
+    //   <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="24" height="24" viewBox="0 0 24 24">
+    //     <path
+    //         d="M10.872 6.831l1.695 3.904 3.654-1.561-1.79 3.426 3.333.954-3.417 1.338 2.231 4.196-4.773-2.582-2.869 2.287.413-3.004-3.792-.726 2.93-1.74-1.885-2.512 3.427.646.843-4.626zm-.786-6.831l-1.665 9.119-6.512-1.228 3.639 4.851-5.548 3.294 7.108 1.361-.834 6.076 5.742-4.577 9.438 5.104-4.288-8.064 6.834-2.677-6.661-1.907 3.25-6.22-6.98 2.982-3.523-8.114z" />
+    // </svg>
+    //   Transcripted: ${filteredColor3}`);
+    // }
   }
 }
